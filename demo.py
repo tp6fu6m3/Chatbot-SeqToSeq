4b5fdaed927168d6120b710d889eba538ebb8d39
tp6fu6m3 <m3tp6fu6@gmail.com>
2021-01-03 21:55:44 +0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+import sys
 import random
 from data.reader import Reader
 from model.model import SeqToSeqModel
@@ -12,5 +12,5 @@ Model.loadWeights('model/model.h5')
 for i in range(10):
     sentence = random.choice(reader.mList_)
     response = Model.predict(sentence, dict)
-    print('[Sample Sentence] {}'.format(sentence))
+    print('[Sample Sentence] {}'.format(sentence).encode(sys.stdin.encoding, 'replace').decode(sys.stdin.encoding))
     print('[Sample Response] {}\n'.format(response))
